589f16ad086ad117b011e1eb2c97b6bbfb601c94
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-09-13 19:09:28 -0400

    Add application components

---
+++ src/components/MovieFilter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="movie-filter"> 
+    <h2>Filter Results</h2>
+    <div class="filter-group">
+      <check-filter v-for="genre in genres" :title="genre" v-on:check-filter="checkFilter"> </check-filter>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  import genres from '../util/genres.js';
+  import CheckFilter from './CheckFilter.vue';
+  export default {
+    data: function(){
+      return{
+        genres
+      }
+    },
+    methods: {
+      checkFilter: function(category, title, checked){
+        this.$emit('check-filter', category, title, checked);
+      }
+    },
+    components: {
+      CheckFilter
+    }
+  }
+</script>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,37 @@
+
+<template>
+    <div id="movie-list"> 
+      <div v-for="movie in filteredMovies" class="movie" :key="movie.title"> {{movie.title}} </div>
+    </div>
+</template>
+
+<script>
+  import genres from '../util/genres.js';
+  export default {
+    data: function(){
+      return {
+        movies: [
+          {'title': 'Pulp fiction', 'genre': genres.CRIME},
+          {'title': 'Jurassic park', 'genre': genres.COMEDY},
+          {'title': 'Avatar', 'genre': genres.COMEDY},
+        ]
+      }
+    },
+    props: ['genre', 'time'],
+    methods:{
+      moviePassesGenreFilter(movie){
+        if(!this.genre.length){
+          return true;
+        }else{
+          return this.genre.find(genre => movie.genre===genre);
+        }
+        
+      }
+    },
+    computed: {
+      filteredMovies(){
+        return this.movies.filter(this.moviePassesGenreFilter);
+      }
+    }
+  }
+</script>
