a41661a2076d055f5bc5b87801c18fc7258a8720
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-09-16 18:55:40 -0400

    Minor update

--- src/components/CheckFilter.vue
+++ src/components/CheckFilter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="{'check-filter': true, 'active': checked }" v-on:click="checkFilter">
+  <div :class="{'check-filter': true, active: checked }" v-on:click="checkFilter">
     <span class="checkbox"></span>
     <span class="check-filter-title">{{title}}</span>
   </div>
